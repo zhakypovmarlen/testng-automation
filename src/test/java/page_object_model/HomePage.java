05996587eade05f75275510e1fbdf979057d0321
marlen <marlenzhakypov@gmail.com>
2023-06-08 20:45:54 -0500

    initial commit

---
+++ src/test/java/page_object_model/HomePage.java
@@ -0,0 +1,12 @@
+package page_object_model;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HomePage {
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+}
---
+++ src/test/java/tests/test1.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utilities.ApplicationFlow;
+import utilities.Driver;
+
+public class test1 {
+
+    @Test
+    public void testing(){
+        Driver.getDriver().get("https://google.com");
+        Faker faker = new Faker();
+        Driver.getDriver().findElement(By.name("q")).sendKeys(faker.harryPotter().character() + Keys.ENTER);
+
+
+
+    }
+}
