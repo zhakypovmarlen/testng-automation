c693df8665e158d151a514203cdac3a01af08a68
marlen <marlenzhakypov@gmail.com>
2023-06-09 19:48:01 -0500

    testing1 added

--- src/test/java/tests/test1.java
+++ src/test/java/tests/test1.java
@@ -14,8 +14,10 @@ public class test1 {
         Driver.getDriver().get("https://google.com");
         Faker faker = new Faker();
         Driver.getDriver().findElement(By.name("q")).sendKeys(faker.harryPotter().character() + Keys.ENTER);
+    }
 
-
-
+    @Test
+    public void testing1(){
+        Driver.getDriver().get("https://etsy.com");
     }
 }
